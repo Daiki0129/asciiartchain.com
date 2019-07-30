5993892fdd5dad2544508bc311a49acebb1c31d0
Daiki Kunii <dai0106ki@gmail.com>
2019-07-31 02:40:50 +0900

    [fix] contract AsciiArtChain

--- contract/contracts/AsciiArtChain.sol
+++ contract/contracts/AsciiArtChain.sol
@@ -1,38 +1,94 @@
 pragma solidity ^0.5.10;
 //関数の実行の最後につけていく
+import "./SafeMath.sol";
 import "./Author.sol";
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721Holder.sol";
+import "./ERC721Full.sol";
+import "./ERC721Holder.sol";
 
 // import "./author_contracts/ownership/Ownable.sol";
 
 contract AsciiArtChain is ERC721Full, ERC721Holder {
-    // using Integers for uint;
+    using SafeMath for uint128;
+    
     address payable public recipientOwner;
-    constructor (address payable _recipientOwner) ERC721Full("AsciiArtChain" ,"ART") public {
-        recipientOwner = _recipientOwner;
+    constructor () ERC721Full("AsciiArtChain" ,"ART") public {
     }
 
     uint public createAsciiArtFee = 0.002 ether;
+    uint internal nextTokenId = 1000000000000;
+    string public tokenURIPrefix = "https://www.asciiartchain.net/metadata/";
+    
+    // string public asciiArt;
 
-    function transferFrom(address from, address to, uint256 tokenId) public payable{
-        //solhint-disable-next-line max-line-length
-        require(_isApprovedOrOwner(msg.sender, tokenId), "ERC721: transfer caller is not owner nor approved");
-        _transferFrom(from, to, tokenId);
-        require(paymentOnlyAuthorFee(tokenId), "ERC721: Failure transfer of Author fee");
+    // function setAsciiArt(string calldata _asciiArt) external {
+    //     asciiArt = _asciiArt;
+    // }
+    // uint16 public constant ASCIIART_TYPE_OFFSET = 10000;
+
+    function setRecipientOwner(address payable _recipientOwner) external onlyOwner {
+        recipientOwner = _recipientOwner;
+    }
+    
+    function setTokenURIPrefix(string calldata _tokenURIPrefix) external onlyOwner {
+        tokenURIPrefix = _tokenURIPrefix;
+    }
+    
+    function setCreateAsciiArtFee(uint _createAsciiArtFee) external onlyOwner {
+        createAsciiArtFee = _createAsciiArtFee;
     }
 
-    function mintAsciiArt(uint _tokenId) public payable {
-        require(msg.value == createAsciiArtFee);
-        _mint(msg.sender, _tokenId);
-        recipientOwner.transfer(createAsciiArtFee);
+    function tokenURI(uint tokenId) external view returns (string memory) {
+        bytes32 tokenIdBytes;
+        if (tokenId == 0) {
+            tokenIdBytes = "0";
+        } else {
+            uint256 value = tokenId;
+            while (value > 0) {
+                tokenIdBytes = bytes32(uint256(tokenIdBytes) / (2 ** 8));
+                tokenIdBytes |= bytes32(((value % 10) + 48) * 2 ** (8 * 31));
+                value /= 10;
+            }
+        }
+
+        bytes memory prefixBytes = bytes(tokenURIPrefix);
+        bytes memory tokenURIBytes = new bytes(prefixBytes.length + tokenIdBytes.length);
+
+        uint8 i;
+        uint8 index = 0;
+        
+        for (i = 0; i < prefixBytes.length; i++) {
+            tokenURIBytes[index] = prefixBytes[i];
+            index++;
+        }
+        
+        for (i = 0; i < tokenIdBytes.length; i++) {
+            tokenURIBytes[index] = tokenIdBytes[i];
+            index++;
+        }
+        
+        return string(tokenURIBytes);
+    }
+    
+    function transferFrom(address from, address to, uint tokenId) public payable{
+      //solhint-disable-next-line max-line-length
+      require(_isApprovedOrOwner(msg.sender, tokenId), "ERC721: transfer caller is not owner nor approved");
+      super._transferFrom(from, to, tokenId);
+    //   require(paymentOnlyAuthorFee(tokenId), "ERC721: Failure transfer of Author fee");
+    }
+//string calldata _message
+    function mintAsciiArt() external payable {
+      require(msg.value == createAsciiArtFee);
+      uint _tokenId = nextTokenId;
+      nextTokenId = nextTokenId.add(1);
+      super._mint(msg.sender, _tokenId);
+    //   super._setTokenURI(_tokenId, _message);
+    //   recipientOwner.transfer(createAsciiArtFee);
     }
 
-    function burnAsciiArt(uint256 tokenId) public payable {
-        //solhint-disable-next-line max-line-length
-        require(_isApprovedOrOwner(msg.sender, tokenId), "ERC721Burnable: caller is not owner nor approved");
-        _burn(tokenId);
-        require(paymentOnlyAuthorFee(tokenId), "ERC721: Failure transfer of Author fee");
+    function burnAsciiArt(uint tokenId) external payable {
+      //solhint-disable-next-line max-line-length
+      require(_isApprovedOrOwner(msg.sender, tokenId), "ERC721Burnable: caller is not owner nor approved");
+      super._burn(tokenId);
+    //   require(paymentOnlyAuthorFee(tokenId), "ERC721: Failure transfer of Author fee");
     }
-
 }
--- contract/contracts/Author.sol
+++ contract/contracts/Author.sol
@@ -8,18 +8,20 @@ contract Author is Ownable {
     using SafeMath for uint256;
 
     uint public initialAuthorFee = 0.00001 ether;
-    // uint public minimumRange = 1;
-    // uint public maximumRange = 5;
-    // uint public totalRange = 10;
 
     mapping(uint => address payable) public tokenToAuthor;
     mapping(uint => uint) public paymentToAuthor;
+    // mapping(address => uint[]) public authorToTokenList;
 
     modifier onlyTokenAuthor(uint _tokenId) {
         require(msg.sender == tokenToAuthor[_tokenId]);
         _;
     }
 
+    function authorOf(uint _tokenId) external view returns (bool){
+        if(tokenToAuthor[_tokenId] == msg.sender) return true;
+        return false;
+    }
     /* setting functions */
     function changeAuthorFee(uint _tokenId, uint _authorFee) public onlyTokenAuthor(_tokenId) returns (bool){
         require(0.01 ether >= _authorFee);
@@ -27,38 +29,14 @@ contract Author is Ownable {
         return true;
     }
 
-    // function changeMinimumRange(uint _minimumRange) public onlyOwner returns (bool){
-    //     require(0 <=_minimumRange);
-    //     minimumRange = _minimumRange;
-    //     return true;
-    // }
-
-    // function changeMaximumRange(uint _maximumRange) public onlyOwner returns (bool){
-    //     require(10 >= _maximumRange);
-    //     maximumRange = _maximumRange;
-    //     return true;
-    // }
-
-    // function changeRangePaymentToAuthor(uint _tokenId, uint _paymentToAuthor) public onlyTokenAuthor(_tokenId) returns (bool){
-    //     require(minimumRange <= _paymentToAuthor && _paymentToAuthor <= maximumRange);
-    //     paymentToAuthor[_tokenId] = _paymentToAuthor;
-    //     return true;
-    // }
-
     function settingOfAuthorFee(uint _tokenId) internal returns (bool){
         tokenToAuthor[_tokenId] = msg.sender;
         paymentToAuthor[_tokenId] = initialAuthorFee;
+        // authorToTokenList[msg.sender].push(_tokenId);
         return true;
     }
-    /* payment functions */
-    // function paymentOwnerAndAuthorFee(uint _tokenId) internal returns (bool){
-    //     require(msg.value == totalFee);
-    //     uint _ownerFee = totalRange.sub(paymentToAuthor[_tokenId]);
-    //     owner().transfer(totalFee.mul(_ownerFee).div(10));
-    //     tokenToAuthor[_tokenId].transfer(totalFee.mul(paymentToAuthor[_tokenId]).div(10));
-    //     return true;
-    // }
 
+    /* payment functions */
     function paymentOnlyAuthorFee(uint _tokenId) public payable returns (bool){
         require(msg.value == paymentToAuthor[_tokenId]);
         tokenToAuthor[_tokenId].transfer(paymentToAuthor[_tokenId]);
